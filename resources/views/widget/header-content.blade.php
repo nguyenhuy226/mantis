c43475eb0a7cdb075bb14a5db763667ff50b2e58
nguyenhuydev <nguyenhuyhuy04041998@gmail.com>
2025-03-26 08:51:27 +0700

    change url image

--- resources/views/widget/header-content.blade.php
+++ resources/views/widget/header-content.blade.php
@@ -456,14 +456,15 @@
         <li class="dropdown pc-h-item header-user-profile">
             <a class="pc-head-link dropdown-toggle arrow-none me-0" data-bs-toggle="dropdown" href="#"
                 role="button" aria-haspopup="false" data-bs-auto-close="outside" aria-expanded="false">
-                <img src="../assets/images/user/avatar-2.jpg" alt="user-image" class="user-avtar">
+                <img src="{{ asset('/images/user/avatar-2.jpg') }}" alt="user-image" class="user-avtar">
                 <span>Stebin Ben</span>
             </a>
             <div class="dropdown-menu dropdown-user-profile dropdown-menu-end pc-h-dropdown">
                 <div class="dropdown-header">
                     <div class="d-flex mb-1">
                         <div class="flex-shrink-0">
-                            <img src="../assets/images/user/avatar-2.jpg" alt="user-image" class="user-avtar wid-35">
+                            <img src="{{ asset('/images/user/avatar-2.jpg') }}" alt="user-image"
+                                class="user-avtar wid-35">
                         </div>
                         <div class="flex-grow-1 ms-3">
                             <h6 class="mb-1">Stebin Ben</h6>
@@ -506,7 +507,7 @@
                             <i class="ti ti-wallet"></i>
                             <span>Billing</span>
                         </a>
-                        <a href="#!" class="dropdown-item">
+                        <a href="{{ route('logout') }}" class="dropdown-item">
                             <i class="ti ti-power"></i>
                             <span>Logout</span>
                         </a>
